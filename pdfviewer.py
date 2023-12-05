50d9b5fc21e9496d73436f55f3f18d378f6ce152
vbob97 <vincenzo.bombace@studenti.unicampania.it>
2023-12-05 14:06:33 +0100

    setup

---
+++ pdfviewer.py
@@ -0,0 +1,16 @@
+# importing everything from tkinter
+from tkinter import *
+# importing ttk for styling widgets from tkinter
+from tkinter import ttk
+# importing filedialog from tkinter
+from tkinter import filedialog as fd
+# importing os module
+import os
+
+class PDFViewer: 
+    def __init__(self, master):
+        pass
+
+root = Tk()
+app = PDFViewer(root)
+root.mainloop()
